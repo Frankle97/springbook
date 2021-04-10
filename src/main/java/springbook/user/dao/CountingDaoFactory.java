102d5143bf8dcb1a66642bbc4d39cb0da416a8d4
Frankle <pairpapa@gmail.com>
2021-04-10 20:26:10 +0900

    mod

--- src/main/java/springbook/user/dao/CountingDaoFactory.java
+++ src/main/java/springbook/user/dao/CountingDaoFactory.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Bean;
 import springbook.user.domain.User;
 
 public class CountingDaoFactory {
-    @Bean
-    public UserDao userDao() {
-        UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
-        return userDao;
-    }
+//    @Bean
+//    public UserDao userDao() {
+//        UserDao userDao = new UserDao();
+//        userDao.setConnectionMaker(connectionMaker());
+//        return userDao;
+//    }
 
     @Bean
     public ConnectionMaker connectionMaker() {
